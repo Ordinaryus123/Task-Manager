3452f3dc62ccfe12d46df3f1324cc27b181170a2
Ordinaryus123 <84708638+Ordinaryus123@users.noreply.github.com>
2024-11-28 11:00:59 +0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+
+import { TasksComponent } from './tasks/tasks.component';
+
+import { TaskComponent } from './tasks/task/task.component';
+import { NewTaskComponent } from './tasks/new-task/new-task.component';
+import { FormsModule } from '@angular/forms';
+import { SharedModule } from './shared/card/shared.module';
+import { TasksModule } from './tasks/tasks.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    UserComponent,
+
+    
+  ],
+  bootstrap: [AppComponent],
+  imports: [BrowserModule,  SharedModule, TasksModule],
+})
+export class AppModule {}
